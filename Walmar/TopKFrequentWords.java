b6b5b8809344242258fcba4cf316f5790b6a798b
Mansi-Raj <mansi.raj.jnv@gmail.com>
2025-01-12 16:09:13 +0530

    add solution of top k frequent words

---
+++ Walmar/TopKFrequentWords.java
@@ -0,0 +1,32 @@
+package Walmar;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TopKFrequentWords {
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String w : words) {
+            map.put(w, map.getOrDefault(w, 0) + 1);
+        }
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
+        list.sort((e1, e2) -> {
+            int compFreq = e2.getValue().compareTo(e1.getValue());
+            if (compFreq != 0) {
+                return compFreq;
+            }
+            return e1.getKey().compareTo(e2.getKey());
+        });
+        List<String> result = new ArrayList<>();
+        int i = 0;
+        for (Map.Entry<String, Integer> e : list) {
+            if (i < k) {
+                result.add(e.getKey());
+                i++;
+            }
+        }
+        return result;
+    }
+}
